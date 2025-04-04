8f0611fb2af3d8b59999a74d4d4cca1d5ff09cbd
Henok Zelalem <z.w.henok@gmail.com>
2025-04-04 10:13:25 +0300

    update

--- script.js
+++ script.js
@@ -34,3 +34,10 @@ window.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+const hamburger = document.getElementById("hamburger");
+const navLinks = document.getElementById("nav-links");
+
+hamburger.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
